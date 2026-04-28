dbba9d7d5d5bd06fdbdfb2dfc0baa48581e77410
Neejy-x <tatsmymail@gmail.com>
2026-04-29 09:12:00 +1000

    project complete

--- index.js
+++ index.js
@@ -13,4 +13,12 @@ const io = socket(server)
 
 io.on('connection', (socket) => {
     console.log('New client connection', socket.id)
+
+    socket.on('chat', (data)=>{
+        io.sockets.emit('chat', data)
+    })
+
+    socket.on('typing', (data)=>{
+       socket.broadcast.emit('typing', data)
+    })
 })
